a4a839eccd5f64b72ef98ffc132dc36273eda813
puemmth <puem.mth@gmail.com>
2022-09-04 20:04:45 +0700

    build

--- comple.cs
+++ comple.cs
@@ -1,174 +1,117 @@
 using System;
-namespace Tree_1
-{
-    class Program
-    {
-        class Node
-        {
+namespace Tree_1 {
+    class Program {
+        class Node {
             public int data { get; set; }
-            public Node LeftNode { get; set; }
-            public Node RightNode { get; set; }
+            public Node Left { get; set; }
+            public Node Right { get; set; }
         }
-        class BinaryTree
-        {
+        class BinaryTree {
             public Node Root { get; set; }
-            public void Insert(int Value)
-            {
+            public Node tmp_node { get; set; }
+            public void Insert(int Value) {
                 Node Parent = null;
                 Node tmp = this.Root;
-                while (tmp != null)
-                {
+                while (tmp != null) {
                     Parent = tmp;
-                    if (Value < tmp.data)
-                    {
-                        tmp = tmp.LeftNode;
-                    }
-                    else if (Value > tmp.data)
-                    {
-                        tmp = tmp.RightNode;
-                    }
-                    else
-                        return;
+                    if (Value < tmp.data) {
+                        tmp = tmp.Left;
+                    } else if (Value > tmp.data) {
+                        tmp = tmp.Right;
+                    } else return;
                 }
                 Node newNode = new Node();
                 newNode.data = Value;
-                newNode.RightNode = null;
-                newNode.LeftNode = null;
-
-                if (this.Root == null)
-                {
+                newNode.Right = null;
+                newNode.Left = null;
+                if (this.Root == null) {
                     this.Root = newNode;
-                }
-                else
-                {
-                    if (newNode.data > Parent.data)
-                        Parent.RightNode = newNode;
-                    else
-                        Parent.LeftNode = newNode;
+                } else {
+                    if (newNode.data > Parent.data) Parent.Right = newNode;
+                    else Parent.Left = newNode;
                 }
                 return;
             }
-            public void PrintTree(Node Parent, String indent, bool last)
-            {
+            public void PrintTree(Node Parent, String indent, bool last) {
                 Console.WriteLine(indent + "+- " + Parent.data);
                 indent += last ? "  " : "|  ";
-                if (Parent.LeftNode != null && Parent.RightNode != null)
+                if (Parent.Left != null && Parent.Right != null)
                 {
-                    PrintTree(Parent.LeftNode, indent, false);
-                    PrintTree(Parent.RightNode, indent, true);
+                    PrintTree(Parent.Left, indent, false);
+                    PrintTree(Parent.Right, indent, true);
                 }
-                else if (Parent.LeftNode != null) { PrintTree(Parent.LeftNode, indent, true); }
-                else if (Parent.RightNode != null) { PrintTree(Parent.RightNode, indent, true); }
+                else if (Parent.Left != null) { PrintTree(Parent.Left, indent, true); }
+                else if (Parent.Right != null) { PrintTree(Parent.Right, indent, true); }
             }
-
-            public Node tmp_node { get; set; }
-            public Node tmp_delete { get; set; }
-            public String LineSpace = "";
-
-            public void Search(int Value)
-            {
+            public void Search(int Value) {
                 Node Parent = null;
                 Node tmp = this.Root;
-                while (tmp != null)
-                {
+                while (tmp != null) {
                     Parent = tmp;
-                    if (Parent.data.Equals(Value))
-                    {
-                        LineSpace += "F";
+                    if (Parent.data.Equals(Value)) {
                         tmp_node = Parent;
                         return;
-                    }
-                    else if (Value < tmp.data)
-                    {
-                        LineSpace += "L";
-                        tmp = tmp.LeftNode;
-                    }
-                    else if (Value > tmp.data)
-                    {
-                        LineSpace += "R";
-                        tmp = tmp.RightNode;
-                    }
-                    else
-                    {
+                    } else if (Value < tmp.data) {
+                        tmp = tmp.Left;
+                    } else if (Value > tmp.data) {
+                        tmp = tmp.Right;
+                    } else {
                         return;
                     }
                 }
             }
-            
-
-            public Node Remove(int key)
+            public Node Remove(int Value)
             {
                 Node parent = null;
                 Node pointer = this.Root;
-                while (pointer != null && pointer.data != key)
-                {
+                while (pointer != null && pointer.data != Value) {
                     parent = pointer;
-                    if (key < pointer.data) {
-                        pointer = pointer.LeftNode;
-                    }
-                    else {
-                        pointer = pointer.RightNode;
-                    }
+                    pointer = (Value < pointer.data) ? pointer.Left : pointer.Right;
                 }
                 if (pointer == null) return this.Root;
-                if (pointer.LeftNode == null && pointer.RightNode == null)
-                {
-                    if (pointer != this.Root)
-                    {
-                        if (parent.LeftNode == pointer) {
-                            parent.LeftNode = null;
+                if (pointer.Left == null && pointer.Right == null) {
+                    if (pointer != this.Root) {
+                        if (parent.Left == pointer) {
+                            parent.Left = null;
+                        } else {
+                            parent.Right = null;
                         }
-                        else {
-                            parent.RightNode = null;
-                        }
-                    }
-                    else {
+                    } else {
                         this.Root = null;
                     }
-                }
-                else if (pointer.LeftNode != null && pointer.RightNode != null)
-                {
-                    Node successor = getMinimumKey(pointer.RightNode);
-                    int val = successor.data;
+                } else if (pointer.Left != null && pointer.Right != null) {
+                    Node successor = Bottom_Leaf(pointer.Right);
+                    // remove data from buttom leaves that are move to node
                     Remove(successor.data);
-                    pointer.data = val;
-                }
-                else {
-                    Node child = (pointer.LeftNode != null)? pointer.LeftNode: pointer.RightNode;
-                    if (pointer != this.Root)
-                    {
-                        if (pointer == parent.LeftNode){
-                            parent.LeftNode = child;
+                    pointer.data = successor.data;
+                } else {
+                    Node child = (pointer.Left != null) ? pointer.Left: pointer.Right;
+                    if (pointer != this.Root) {
+                        if (pointer == parent.Left){
+                            parent.Left = child;
+                        } else {
+                            parent.Right = child;
                         }
-                        else {
-                            parent.RightNode = child;
-                        }
-                    }
-                    else {
+                    } else {
                         this.Root = child;
                     }
                 }
                 return this.Root;
             }
-    
-            public Node getMinimumKey(Node pointer)
-            {
-                while (pointer.LeftNode != null) {
-                    pointer = pointer.LeftNode;
+            public Node Bottom_Leaf(Node pointer) {
+                while (pointer.Left != null) {
+                    pointer = pointer.Left;
                 }
                 return pointer;
             }
-
         }
-
-       static void Main(string[] args)
-        {
+       public static void Main(string[] args) {
             BinaryTree BT = new BinaryTree();
             int[] keys = { 50, 20, 70, 30, 110, 10, 15, 60, 90, 150 };
             foreach (int i in keys){
                 BT.Insert(i);
             }
-            BT.PrintTree(BT.Root, "Remov    ", true);
+            BT.PrintTree(BT.Root, "Leaf     ", true);
             BT.Remove(10);
             BT.PrintTree(BT.Root, "After    ", true);
         }
